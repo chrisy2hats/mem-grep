0a1273ea153a787c1c63ae93f6fd5e8f69239856
chrisfoster4 <chrismarkfoster@yahoo.co.uk>
2019-12-13 14:07:54 +0000

    Add class structure to BssSearcher>

--- src/bss-searcher.cpp
+++ src/bss-searcher.cpp
@@ -3,44 +3,91 @@
 using std::cout;
 using std::endl;
 
-[[nodiscard]] inline bool AddrIsOnHeap(const void *addr, const void *heapStart, const void *heapEnd) {
+BssSearcher::BssSearcher(const char *bssStart, const size_t &bssSize, const char *actualBssStart) {
+    bssSize_ = bssSize;
+    bssStart_ = bssStart;
+    actualBssStart_ = actualBssStart;
+}
+
+[[nodiscard]] bool BssSearcher::AddrIsOnHeap(const void *addr, const void *heapStart, const void *heapEnd) const {
     const bool IsOnHeap = addr >= heapStart && addr <= heapEnd;
     return IsOnHeap;
 }
 
-void findBssHeapPointers(char *block, size_t block_size, char *actualBSSAddr, void *heapStart, void *heapEnd) {
-    auto matches = std::vector<SearchMatch>();
+std::vector<RemoteHeapPointer> BssSearcher::findHeapPointers(const MAPS_ENTRY &heap) {
+    auto matches = std::vector<RemoteHeapPointer>();
     size_t zeroCount = 0, onHeapCount = 0, offHeapCount = 0;
-    for (size_t i = 0; i < block_size; i += (sizeof(void *))) {
+    for (size_t i = 0; i < bssSize_; i += (sizeof(void *))) {
         size_t current = 0;
-        memcpy(&current, block + i, sizeof(void *));
+        memcpy(&current, bssStart_ + i, sizeof(void *));
         if (current == 0) {
             zeroCount++;
             continue;
         }
 
-        //If our target programs source has, in global scope, "int* i = new int;"
-        //pointerLocation will be &i
-        //And addressPointerTo will be i (The memory address potentially on the heap)
-        //Except within our deepcopy of the processes .bss section not the actual .bss section
-        auto pointerLocation = (void **) (block + i);
+        // If our target programs source has, in global scope, "int* i = new int;"
+        // pointerLocation will be &i
+        // And addressPointerTo will be i (The memory address potentially on the heap
+        // Except within our deepcopy of the processes .bss section not the
+        // actual .bss section
+        auto pointerLocation = (void **) (bssStart_ + i);
         auto addressPointedTo = (void *) *pointerLocation;
 
-        if (AddrIsOnHeap(addressPointedTo, heapStart, heapEnd)) {
-            const void *actualAddr = actualBSSAddr + i;
+        if (AddrIsOnHeap(addressPointedTo, heap.start, heap.end)) {
+            void *actualAddr =(void*) (actualBssStart_ + i);
             cout << "---------------------------\n";
             cout << "Global pointer to heap memory found\n";
             cout << "Pointer memory is at: " << actualAddr << "\n";
             cout << "Which points to : " << addressPointedTo << "\n";
             cout << "Pointer found at .bss offset:" << i << "\n";
             cout << "---------------------------\n";
+            struct RemoteHeapPointer result = {actualAddr, addressPointedTo};
+            matches.push_back(result);
+
             onHeapCount++;
         } else {
             offHeapCount++;
-        };
+        }
     }
-
-    cout << "Of " << block_size / sizeof(void *) << " locations checked\n" << zeroCount <<
-              " where 0 (NULL/nullptr)\n" << onHeapCount << " locations point to the heap\n" << " and "
-              << offHeapCount << " non zero values that point elsewhere\n";
+    return matches;
 }
+
+/*  From the pointers found in the .bss read the data pointed to
+ *  Attempt to findout what it points to
+ *  Also search for additional pointers to follow
+ *  NOTE it is assumed that stucts are packed. This means every pointer will be
+ * 8 bytes aligned A process MAY use unpacked structs however this is unlikely
+ * as it is worse for performance while saving a few bytes of memory
+ */
+
+std::vector<RemoteHeapPointer> BssSearcher::traverseHeapPointers(const pid_t& pid, const struct MAPS_ENTRY& heap, std::vector<RemoteHeapPointer> heapPointers) {
+
+    void *current;
+    for (const RemoteHeapPointer& i : heapPointers) {
+        std::vector<RemoteHeapPointer> currentLayerPointers;
+        // Get the size of the block pointed to
+        size_t blockSize = getMallocMetaData(i.pointsTo, pid);
+
+        // Get a deep copy of that area pointed to
+        char *blockPointedTo = deepCopy(pid, i.pointsTo, blockSize);
+
+        // Traverse it for additional pointers. Push to pointers and recurse call
+        for (size_t j = 0; j < blockSize; j += (sizeof(void *))) {
+            memcpy(&current, blockPointedTo + j, sizeof(void *));
+
+            if (AddrIsOnHeap(current, heap.start, heap.end)) {
+                void *actualAddr = (char *)i.pointsTo + j;
+                auto pointerLocation = (void **)(blockPointedTo + j);
+                auto addressPointedTo = (void *)*pointerLocation;
+
+                struct RemoteHeapPointer p = {actualAddr, addressPointedTo};
+                currentLayerPointers.push_back(p);
+            }
+        }
+        auto lowerPointers = traverseHeapPointers(pid, heap,currentLayerPointers);
+        heapPointers.insert(heapPointers.end(), lowerPointers.begin(), lowerPointers.end());
+        delete[] blockPointedTo;
+    }
+    return heapPointers;
+}
+
--- src/bss-searcher.hpp
+++ src/bss-searcher.hpp
@@ -4,6 +4,33 @@
 #include <iostream>
 #include <cstring>
 #include "memory-searcher.hpp"
+#include "map-parser.hpp"
+#include "deep-copy.hpp"
+#include "malloc-metadata.hpp"
 
-void findBssHeapPointers(char* block, size_t block_size, char* actualBSSAddr, void* heapStart, void*heapEnd);
+// A pointer to heap memory in the remote process
+struct RemoteHeapPointer
+{
+  void *actualAddress;
+  void *pointsTo;  // The heap location pointed to
+};
+
+class BssSearcher{
+private:
+    [[nodiscard]] bool AddrIsOnHeap(const void *addr, const void *heapStart, const void *heapEnd) const;
+    const char* bssStart_;
+    size_t bssSize_;
+    const char* actualBssStart_;
+public:
+
+    //Heap start and heap end can't be constructor parameters as they may changed throughout the lifetime of the object.
+    //The heap can grow and shrink at any time
+    //The caller MAY have a more up to date size of the heap
+    //Bss Size and location however won't change during runtime as .bss is a fixed size at compile time of the remote program
+    BssSearcher(const char *bssStart, const size_t &bssSize, const char *actualBssStart);
+
+
+    std::vector<RemoteHeapPointer> findHeapPointers(const MAPS_ENTRY& heap);
+    std::vector<RemoteHeapPointer> traverseHeapPointers( const pid_t& pid, const MAPS_ENTRY& heap,std::vector<RemoteHeapPointer> heapPointers);
+};
 #endif
