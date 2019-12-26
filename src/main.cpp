2276218d87fb5191d1b6197a7132bd26a81d6d2f
chrisfoster4 <chrismarkfoster@yahoo.co.uk>
2019-12-26 14:47:34 +0000

    Update main to utilise new map parsing and new argument parsing
    Logic has been moved out of main to the functions it calls where possible reducing main() to about 40 lines.
    The updated main also reflects changes in BssSearchers constructor.

--- src/main.cpp
+++ src/main.cpp
@@ -1,58 +1,55 @@
 #include "map-parser.hpp"
 #include "bss-searcher.hpp"
-#include "deep-copy.hpp"
 #include "prerun-checks.hpp"
+#include "memory-searcher.cpp"
+#include "stack-searcher.hpp"
+#include "heap-traverser.hpp"
+#include "argument-parsing.hpp"
 
 int main(int argc, char **argv) {
-    auto pid = 0;
-    if (argc == 3) {
-        if (strcmp("--pid", argv[1]) == 0) {
-            pid = std::stoul(argv[2]);
-            std::cout << "PID parsed as:" << pid << std::endl;
-        } else {
-            std::cout << "ERR: 1st arg isn't --pid it is" << argv[1] << std::endl;
-            exit(1);
-        }
-    } else {
-        std::cout << "PID not provided.\n";
-        exit(1);
-    }
-    if (!PreRunCheck()){
-        std::cout << "Prerun checks failed. mem-grep will now exit. Look at above output for info\n";
+    const struct cliArgs userArgs = ArgumentParser::parseArguments(argc, argv);
+
+    if (!PreRunCheck()) {
+        std::cerr << "Prerun checks failed. mem-grep will now exit. Look at above output for info\n";
         exit(1);
     }
 
-    const auto entries = ParseMap(pid);
+    auto parser = MapParser(userArgs.pid);
+    const auto entries = parser.ParseMap();
 
-    MAPS_ENTRY stack = {};
-    MAPS_ENTRY heapMetadata = {};
-    MAPS_ENTRY bss = {};
-    MAPS_ENTRY data = {};
-    for (const auto& entry : entries) {
-        if (entry.file_path == "[stack]") {
-            stack = entry;
-        }
-        if (entry.file_path == "[heap]") {
-            heapMetadata = entry;
-        }
-        if (entry.file_path == ".bss") {
-            bss = entry;
-        }
-        if (entry.file_path == ".data") {
-            data = entry;
+    MAPS_ENTRY stack = parser.getStoredStack();
+    MAPS_ENTRY heapMetadata = parser.getStoredHeap();
+    MAPS_ENTRY bss = parser.getStoredBss();
+    MAPS_ENTRY data = parser.getStoredData();
+    MAPS_ENTRY text = parser.getStoredText();
+
+    if (userArgs.SearchBss) {
+        const auto bssSearcher = BssSearcher((char *) bss.start, (char *) bss.end, userArgs.pid);
+        const auto heapPointersInBss = bssSearcher.findHeapPointers(heapMetadata);
+
+        std::cout << "Found " << heapPointersInBss.size() << "pointers to the heap from the .bss section\n";
+        if (userArgs.TraverseBssPointers) {
+            const auto traversed = HeapTraverser::traverseHeapPointers(heapMetadata, heapPointersInBss, userArgs.pid);
+            std::cout << "From " << heapPointersInBss.size() << " a further "
+                      << HeapTraverser::heapPointersKnown(traversed) - heapPointersInBss.size()
+                      << " heap pointers where found by traversing\n";
+            HeapTraverser::printHeap(traversed);
         }
     }
+    if (userArgs.SearchStack) {
+        const auto stackSearcher = StackSearcher(stack.start, text, userArgs.pid);
+        const auto heapPointersOnStack = stackSearcher.findHeapPointers(stack.end, heapMetadata,
+                                                                        userArgs.StackFramesToSearch);
+        std::cout << "Found " << heapPointersOnStack.size() << " pointers to the heap on the stack\n";
+        if (userArgs.TraverseStackPointers) {
 
-    const auto bssSize = (char *) bss.end - (char *) bss.start;
-
-    const char *bssCopy = deepCopy(pid, bss.start, bssSize);
-    auto bssAnalysis = BssSearcher(bssCopy, bssSize, (char *) bss.start,pid);
-    const std::vector<RemoteHeapPointer> heapPointers = bssAnalysis.findHeapPointers(heapMetadata);
-    const std::vector<RemoteHeapPointer> deepLayerPointers = bssAnalysis.traverseHeapPointers(heapMetadata, heapPointers);
-    delete[] bssCopy;
-
-    for (auto i : deepLayerPointers) {
-        std::cout << i.pointsTo << " : " << i.actualAddress << " : " << i.sizePointedTo << "\n";
+            const auto deepStackPointers = HeapTraverser::traverseHeapPointers(heapMetadata, heapPointersOnStack,
+                                                                               userArgs.pid);
+            std::cout << "From " << heapPointersOnStack.size() << " a further "
+                      << HeapTraverser::heapPointersKnown(deepStackPointers) - heapPointersOnStack.size()
+                      << " heap pointers where found by traversing\n";
+            HeapTraverser::printHeap(deepStackPointers);
+        }
     }
 
     std::cout << "mem-grep finished without an error.\n";
