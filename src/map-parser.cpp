a4eed0a97e51b38b0647d380ad93450ab939c4c7
chrisfoster4 <chrismarkfoster@yahoo.co.uk>
2019-12-31 15:44:47 +0000

    Alter maps parsing to take the permissions flags into account
    Each line of the maps file is checked if it meets the criteria of different categories such as is it a heap, stack, bss and others.
    Individual functions have been added to check if a given MAPS_ENTRY is of a certain category.

--- src/map-parser.cpp
+++ src/map-parser.cpp
@@ -5,52 +5,57 @@ using std::endl;
 using std::string;
 
 MapParser::MapParser(const pid_t pid) :
-        pid_(pid) {}
+        pid_(pid) {
+    executable_path_ = GetExecutablePath();
+}
+
+std::string MapParser::GetExecutablePath() {
+    const string exe_containing_file = "/proc/" + std::to_string(pid_) + "/exe";
+    std::cout << "Obtaining executable location from:" << exe_containing_file << "\n";
+
+    char* buffer=new char[PATH_MAX];
+    ssize_t nread = readlink(exe_containing_file.c_str(), buffer, PATH_MAX);
+    assert(nread!=0);
+    if (errno){
+        std::cerr << "Unhandled error whilst getting location inside:" << exe_containing_file << "\n";
+        exit(1);
+    }
+
+    const std::string exe_path(buffer);
+    assert(!exe_path.empty());
+    assert(exe_path.at(0)=='/');
+
+    delete[] buffer;
+    return exe_path;
+}
 
 [[nodiscard]] std::vector<struct MAPS_ENTRY> MapParser::ParseMap() {
 
-    const string maps_path = "/proc/" + std::to_string(pid_) + "/maps";
+  const string maps_path = "/proc/" + std::to_string(pid_) + "/maps";
 
-    std::ifstream maps_file(maps_path);
-    if (!maps_file) {
-        std::cerr << "Failed to open file: " << maps_path << endl;
-        abort();
-    }
-    cout << "Parsing file:" << maps_path << endl;
+  std::ifstream maps_file(maps_path);
+  if (!maps_file) {
+    std::cerr << "Failed to open file: " << maps_path << endl;
+    abort();
+  }
+  cout << "Parsing file:" << maps_path << endl;
 
-    std::vector<MAPS_ENTRY> entries = {};
-    string line;
-    size_t lineCount = 1;
-    while (std::getline(maps_file, line)) {
-        MAPS_ENTRY mapEntry = ParseLine(line);
-        if (mapEntry.file_path == "[stack]") {
-            stack_ = mapEntry;
-        } else if (mapEntry.file_path == "[heap]") {
-            heap_ = mapEntry;
-        } else {
-            switch (lineCount) {
-                case 1:
-                    text_=mapEntry;
-                    break;
-                case 2:
-                    data_ = mapEntry;
-                    mapEntry.file_path = ".data";
-                    break;
-                case 3:
-                    bss_ = mapEntry;
-                    mapEntry.file_path = ".bss";
-                    break;
-                default:
-                    break;
-            }
-        }
-        entries.push_back(mapEntry);
-        lineCount++;
-    }
-    maps_file.close();
-    return entries;
+  std::vector<MAPS_ENTRY> entries = {};
+  string line;
+  while (std::getline(maps_file, line)) {
+    MAPS_ENTRY entry = ParseLine(line);
+    if (IsBssEntry(entry)) bss_=entry;
+    if (IsHeapEntry(entry))heap_=entry;
+    if (IsStackEntry(entry))stack_=entry;
+    if (IsDataEntry(entry))data_=entry;
+    if (IsTextEntry(entry))text_sections_.push_back(entry);
+    if (IsMmapEntry(entry))mmap_sections_.push_back(entry);
+
+    entries.push_back(entry);
+  }
+  maps_file.close();
+  return entries;
 }
-
 
 // Parse a single line of a /dev/PID/maps.
 // The lines have the following format
@@ -58,42 +63,93 @@ MapParser::MapParser(const pid_t pid) :
 //    00400000-00452000 r-xp 00000000 08:02 173521      /usr/bin/dbus-daemon
 struct MAPS_ENTRY MapParser::ParseLine(const std::string &line) const {
 //	std::cout << "line=" << line<< std::endl;
-    struct MAPS_ENTRY mapEntry = {};
+  struct MAPS_ENTRY mapEntry = {};
 
-    if (line.empty())
-        return mapEntry;
-
-    std::vector<size_t> spaces = {};
-    for (size_t i = 0; i < line.length(); i++) {
-        if (line[i] == ' ') {
-            spaces.emplace_back(i + 1);
-        }
-    }
-    //A valid maps line will have atleast 5 spaces in it and contain a '-'
-    if (spaces.size() < 5) {
-        return mapEntry;
-    }
-
-    const auto first_dash = line.find("-");
-    if (first_dash == string::npos)
-        return mapEntry;
-
-    const string start_str = line.substr(0, first_dash);
-    const uint64_t start = std::stoul(start_str, 0, 16);
-    memcpy(&mapEntry.start, &start, sizeof(void *)); //UGLY
-
-    const string end_str = line.substr(first_dash + 1, spaces[0]);
-    const uint64_t end = std::stoul(end_str, 0, 16);
-    memcpy(&mapEntry.end, &end, sizeof(void *)); //UGLY
-
-    mapEntry.permissions = line.substr(spaces[0], spaces[1] - spaces[0] - 1);
-    mapEntry.offset = line.substr(spaces[1], spaces[2] - spaces[1] - 1);
-    mapEntry.device = line.substr(spaces[2], spaces[3] - spaces[2] - 1);
-    mapEntry.inode = line.substr(spaces[3], spaces[4] - spaces[3] - 1);
-
-    string file_path = line.substr(spaces[4]);
-    file_path.erase(std::remove(file_path.begin(), file_path.end(), ' '), file_path.end());
-    mapEntry.file_path = file_path;
-
+  if (line.empty())
     return mapEntry;
+
+  std::vector<size_t> spaces = {};
+  for (size_t i = 0; i < line.length(); i++) {
+    if (line[i] == ' ') {
+      spaces.emplace_back(i + 1);
+    }
+  }
+  //A valid maps line will have atleast 5 spaces in it and contain a '-'
+  if (spaces.size() < 5) {
+    return mapEntry;
+  }
+
+  const auto first_dash = line.find("-");
+  if (first_dash == string::npos)
+    return mapEntry;
+
+  const string start_str = line.substr(0, first_dash);
+  const uint64_t start = std::stoul(start_str, 0, 16);
+  memcpy(&mapEntry.start, &start, sizeof(void *)); //UGLY
+
+  const string end_str = line.substr(first_dash + 1, spaces[0]);
+  const uint64_t end = std::stoul(end_str, 0, 16);
+  memcpy(&mapEntry.end, &end, sizeof(void *)); //UGLY
+
+  mapEntry.permissions = line.substr(spaces[0], spaces[1] - spaces[0] - 1);
+  mapEntry.offset = line.substr(spaces[1], spaces[2] - spaces[1] - 1);
+  mapEntry.device = line.substr(spaces[2], spaces[3] - spaces[2] - 1);
+  mapEntry.inode = line.substr(spaces[3], spaces[4] - spaces[3] - 1);
+
+  string file_path = line.substr(spaces[4]);
+  file_path.erase(std::remove(file_path.begin(), file_path.end(), ' '), file_path.end());
+  mapEntry.file_path = file_path;
+
+  return mapEntry;
 }
+
+
+bool MapParser::IsExecutable(const MAPS_ENTRY& entry)const {
+  return entry.permissions.find('x')!=std::string::npos;
+}
+
+bool MapParser::IsReadable(const MAPS_ENTRY& entry)const {
+    return entry.permissions.find('r')!=std::string::npos;
+
+}
+bool MapParser::IsWriteable(const MAPS_ENTRY& entry)const {
+    return entry.permissions.find('w')!=std::string::npos;
+
+}
+bool MapParser::IsPrivate(const MAPS_ENTRY& entry) const{
+    return entry.permissions.find('p')!=std::string::npos;
+
+}
+
+//Text entries MUST be executable,readable and have the file_path of the binary on the filesystem
+//For instance this is a valid .text entry:
+//55d3cab00000-55d3cabac000 r-xp 00000000 fd:01 3670594                    /usr/bin/kate
+//But this isn't (notice not executable)
+//55d3cadab000-55d3cadb3000 r--p 000ab000 fd:01 3670594                    /usr/bin/kate
+bool MapParser::IsTextEntry(const MAPS_ENTRY &entry) const {
+    return IsReadable(entry) && IsExecutable(entry) && entry.file_path == executable_path_;
+}
+
+bool MapParser::IsHeapEntry(const MAPS_ENTRY &entry) const {
+    return IsReadable(entry) && IsWriteable(entry) && (!IsExecutable(entry)) && entry.file_path == "[heap]";
+}
+
+bool MapParser::IsStackEntry(const MAPS_ENTRY &entry) const {
+    return IsReadable(entry) && IsWriteable(entry) && !IsExecutable(entry) && entry.file_path == "[stack]";
+}
+
+//Mmaped regions don't have a file path associated
+bool MapParser::IsMmapEntry(const MAPS_ENTRY &entry) const {
+    return entry.file_path.empty();
+}
+
+bool MapParser::IsBssEntry(const MAPS_ENTRY &entry) const {
+    return IsReadable(entry) && IsWriteable(entry) && !IsExecutable(entry) && entry.file_path == executable_path_;
+}
+
+bool MapParser::IsDataEntry(const MAPS_ENTRY &entry) const {
+    //TODO need to work out the characteristics of .data sections
+    return false;
+}
+
+
--- src/map-parser.hpp
+++ src/map-parser.hpp
@@ -7,41 +7,81 @@
 #include <cstring>
 #include <vector>
 #include <algorithm>
+#include <sstream>
+#include <cassert>
+#include <unistd.h>
+#include <climits> //For PATH_MAX constant
+
+// Representing a single line of the /proc/PID/maps file in a struct
+// There will be as many MAPS_ENTRYs as lines in the maps file
+// This is what the maps file looks like
+// address           perms offset  dev   inode       pathname
+// 00400000-00452000 r-xp 00000000 08:02 173521      /usr/bin/dbus-daemon
+// 00651000-00652000 r--p 00051000 08:02 173521      /usr/bin/dbus-daemon
+// 00652000-00655000 rw-p 00052000 08:02 173521      /usr/bin/dbus-daemon
+// 00e03000-00e24000 rw-p 00000000 00:00 0           [heap]
+// 00e24000-011f7000 rw-p 00000000 00:00 0           [heap]
 
 struct MAPS_ENTRY {
-    void *start;
-    void *end;
-    std::string permissions;
-    std::string offset;
-    std::string device;
-    std::string inode;
-    std::string file_path;
+  void *start;
+  void *end;
+  std::string permissions;
+  std::string offset;
+  std::string device;
+  std::string inode;
+  std::string file_path;
 };
 
 class MapParser {
-private:
-    const pid_t pid_;
-    struct MAPS_ENTRY stack_;
-    struct MAPS_ENTRY heap_;
-    struct MAPS_ENTRY bss_;
-    struct MAPS_ENTRY data_;
-    struct MAPS_ENTRY text_;
+ public:
+  explicit MapParser(pid_t pid);
 
+  [[nodiscard]] std::vector<struct MAPS_ENTRY> ParseMap();
+  [[nodiscard]] struct MAPS_ENTRY getStoredStack() const { return stack_; }
+
+  // TODO a program can have multiple heaps if it is multi-threaded. This function needs to return a std::vector of heaps
+  [[nodiscard]] struct MAPS_ENTRY getStoredHeap() const { return heap_; }
+  [[nodiscard]] struct MAPS_ENTRY getStoredBss() const { return bss_; }
+  [[nodiscard]] struct MAPS_ENTRY getStoredData() const { return data_; }
+
+  [[nodiscard]] std::vector<struct MAPS_ENTRY> getStoredText() const { return text_sections_; }
+
+  [[nodiscard]] std::vector<struct MAPS_ENTRY> getStoredMmap() const { return mmap_sections_; }
+
+ protected:
+ private:
+  const pid_t pid_;
+
+  struct MAPS_ENTRY stack_;
+  struct MAPS_ENTRY heap_;
+  struct MAPS_ENTRY bss_;
+  struct MAPS_ENTRY data_;
+  std::vector<struct MAPS_ENTRY> text_sections_;
+  std::vector<struct MAPS_ENTRY> mmap_sections_;
+  std::string executable_path_;
+
+  //Rudimentary functions that simply check for a certain character in the MAPS_ENTRY.permissions string
+  [[nodiscard]] bool IsExecutable(const MAPS_ENTRY &entry) const;
+  [[nodiscard]] bool IsReadable(const MAPS_ENTRY &entry) const;
+  [[nodiscard]] bool IsWriteable(const MAPS_ENTRY &entry) const;
+  [[nodiscard]] bool IsPrivate(const MAPS_ENTRY &entry) const;
+
+  //Higher level functions returning a combination of the above 4 functions and checking the MAPS_ENTRY.file_path
+  [[nodiscard]] bool IsDataEntry(const MAPS_ENTRY &entry) const;
+  [[nodiscard]] bool IsTextEntry(const MAPS_ENTRY &entry) const;
+  [[nodiscard]] bool IsHeapEntry(const MAPS_ENTRY &entry) const;
+  [[nodiscard]] bool IsStackEntry(const MAPS_ENTRY &entry) const;
+  [[nodiscard]] bool IsMmapEntry(const MAPS_ENTRY &entry) const;
+  [[nodiscard]] bool IsBssEntry(const MAPS_ENTRY &entry) const;
+
+  //Obtain the path of the binary being executed from /proc/PID/exe
+  std::string GetExecutablePath();
+
+  // Allows for ParseLine to be unit tested. In normal compiles this is not defined so doesn't break encapsulation
+  // If you try to access ParseLine in the source code you will get errors saying attempting to access private method
 #ifdef UNIT_TEST
-public:
+ public:
 #endif
-    [[nodiscard]] struct MAPS_ENTRY ParseLine(const std::string &line) const;
-
-public:
-    explicit MapParser(pid_t pid);
-
-    [[nodiscard]] struct MAPS_ENTRY getStoredStack() const { return stack_; }
-    [[nodiscard]] struct MAPS_ENTRY getStoredHeap() const { return heap_; }
-    [[nodiscard]] struct MAPS_ENTRY getStoredBss() const { return bss_; }
-    [[nodiscard]] struct MAPS_ENTRY getStoredData() const { return data_; }
-    [[nodiscard]] struct MAPS_ENTRY getStoredText() const { return text_; }
-
-    [[nodiscard]] std::vector<struct MAPS_ENTRY> ParseMap();
+  [[nodiscard]] struct MAPS_ENTRY ParseLine(const std::string &line) const;
 };
-
 #endif
