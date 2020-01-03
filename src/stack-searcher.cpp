9931ef6f612482505a37de1eb463af5309e2cea5
chrisfoster4 <chrismarkfoster@yahoo.co.uk>
2020-01-03 12:07:14 +0000

    Don't fetch the size pointed to for pointers to the start of the heap
    There is a large whitespace changes in this commit to bring the indentation inline with Google C++ guidelines. The only actual changes are on lines 64 to 76
    
    Pointers to the very first byte of the heap would previously make the program malfunction when we attempted to get the size they pointed to via GetMallocMetadata.
    
    This was due to reading the 8 bytes before the pointer which would not actually be part of the heap and may not belong to the process specified in the PID parameter of process_vm_readv. This would cause process_vm_readv to return a Bad Address error.
    
    The fix is to not fetch their size via GetMallocMetadata but just assign them a size of 0.

--- src/stack-searcher.cpp
+++ src/stack-searcher.cpp
@@ -2,79 +2,95 @@
 
 using std::cout;
 
-StackSearcher::StackSearcher(const void *stackStart, const MAPS_ENTRY &text, const pid_t pid, const size_t max_heap_obj)
-        : stackStart_(stackStart),
-          textStart_(text.start),
-          textEnd_(text.end),
-          textSize_((char *) text.start - (char *) text.end),
-          pid_(pid),
-          max_heap_obj_(max_heap_obj) {}
-
-//If the address is within the .text part of the target binary
-[[nodiscard]]bool StackSearcher::AddrIsInText(const void *addr) const {
-    const bool IsInText = addr >= textStart_ && addr <= textEnd_;
-    return IsInText;
+StackSearcher::StackSearcher(const void *stackStart, const MAPS_ENTRY &text, const pid_t pid, const size_t max_heap_obj) :
+		stackStart_(stackStart),
+		textStart_(text.start),
+		textEnd_(text.end),
+		textSize_((char *)text.start - (char *)text.end),
+		pid_(pid),
+		max_heap_obj_(max_heap_obj)
+{
 }
 
-[[nodiscard]] bool StackSearcher::AddrIsOnHeap(const void *addr, const void *heapStart, const void *heapEnd) const {
-    const bool IsOnHeap = addr >= heapStart && addr <= heapEnd;
-    return IsOnHeap;
+// If the address is within the .text part of the target binary
+[[nodiscard]] bool StackSearcher::AddrIsInText(const void *addr) const
+{
+  const bool IsInText = addr >= textStart_ && addr <= textEnd_;
+  return IsInText;
 }
 
-std::vector<RemoteHeapPointer>
-StackSearcher::findHeapPointers(const void *curStackEnd, const MAPS_ENTRY &heap, size_t framesToSearch) const {
+[[nodiscard]] bool StackSearcher::AddrIsOnHeap(const void *addr, const void *heapStart, const void *heapEnd) const
+{
+  const bool IsOnHeap = addr >= heapStart && addr <= heapEnd;
+  return IsOnHeap;
+}
 
-    if (framesToSearch == 0) {
-        framesToSearch = UINTMAX_MAX;
+std::vector<RemoteHeapPointer> StackSearcher::findHeapPointers(const void *curStackEnd, const MAPS_ENTRY &heap, size_t framesToSearch) const
+{
+  if (framesToSearch == 0) {
+    framesToSearch = UINTMAX_MAX;
+  }
+  size_t framesSearched = 0;
+
+  const size_t curStackSize = (char *)curStackEnd - (char *)stackStart_;
+  const char *stackCopy = DeepCopy(pid_, stackStart_, curStackSize);
+
+  auto matches = std::vector<RemoteHeapPointer>();
+  size_t zeroCount = 0;
+  for (size_t i = 0; i < curStackSize; i += (sizeof(void *))) {
+    size_t current = 0;
+    memcpy(&current, (char *)stackCopy + i, sizeof(void *));
+    if (current == 0) {
+      zeroCount++;
+      continue;
     }
-    size_t framesSearched = 0;
 
-    const size_t curStackSize = (char *) curStackEnd - (char *) stackStart_;
-    const char *stackCopy = DeepCopy(pid_, stackStart_, curStackSize);
-
-    auto matches = std::vector<RemoteHeapPointer>();
-    size_t zeroCount = 0;
-    for (size_t i = 0; i < curStackSize; i += (sizeof(void *))) {
-        size_t current = 0;
-        memcpy(&current, (char *) stackCopy + i, sizeof(void *));
-        if (current == 0) {
-            zeroCount++;
-            continue;
-        }
-
-
-        /*If our target programs source has a function that has a pointer to a heap object
-        Like
-        void foobar(){
-            int* x = new int;
-        }
-        then pointerLocation is &x and addressPointedTo is x
-        */
-        auto pointerLocation = (void **) ((char *) stackCopy + i);
-        auto addressPointedTo = (void *) *pointerLocation;
-
-
-        if (AddrIsOnHeap(addressPointedTo, heap.start, heap.end)) {
-            void *actualAddr = (void *) ((char *) stackStart_ + i);
-            size_t sizePointedTo = GetMallocMetadata(addressPointedTo, pid_, max_heap_obj_, true);
-            cout << "---------------------------\n";
-            cout << "Global pointer to heap memory found\n";
-            cout << "Pointer memory is at: " << actualAddr << "\n";
-            cout << "Which points to : " << addressPointedTo << "\n";
-            cout << "Pointer found at stack offset:" << i << "\n";
-            cout << "Which points to block of size:" << sizePointedTo << "\n";
-            cout << "---------------------------\n";
-            const struct RemoteHeapPointer result = {actualAddr, addressPointedTo, sizePointedTo};
-            matches.push_back(result);
-
-        } else if (AddrIsInText(addressPointedTo)) {
-            std::cout << "Frame end found return addr to :" << addressPointedTo << "\n";
-            framesSearched++;
-            if (framesSearched > framesToSearch) {
-                break;
-            }
-        }
+    /*If our target programs source has a function that has a pointer to a heap object
+    Like
+    void foobar(){
+	int* x = new int;
     }
-    delete[] stackCopy;
-    return matches;
+    then pointerLocation is &x and addressPointedTo is x
+    */
+    auto pointerLocation = (void **)((char *)stackCopy + i);
+    auto addressPointedTo = (void *)*pointerLocation;
+
+    // We have to handle pointers to the first byte of the heap differently
+    // If we try and access the byte before the pointer to get the size via GetMallocMetadata
+    // We may be accessing memory that doesn't belong to the same segment which can cause this program to SEGFAULT.
+    if (AddrIsOnHeap(addressPointedTo, heap.start, heap.end)) {
+      void *actualAddr = (void *)((char *)stackStart_ + i);
+
+      if (addressPointedTo == heap.start) {
+	cout << "---------------------------\n";
+	cout << "Stack pointer to the start of the heap found\n";
+	cout << "Pointer memory is at: " << actualAddr << "\n";
+	cout << "Which points to : " << addressPointedTo << "\n";
+	cout << "Pointer found at stack offset:" << i << "\n";
+	cout << "---------------------------\n";
+	const struct RemoteHeapPointer result = {actualAddr, addressPointedTo, .sizePointedTo = 0};
+	matches.push_back(result);
+      } else {
+	size_t sizePointedTo = GetMallocMetadata(addressPointedTo, pid_, max_heap_obj_, true);
+	cout << "---------------------------\n";
+	cout << "Stack pointer to heap memory found\n";
+	cout << "Pointer memory is at: " << actualAddr << "\n";
+	cout << "Which points to : " << addressPointedTo << "\n";
+	cout << "Pointer found at stack offset:" << i << "\n";
+	cout << "Which points to block of size:" << sizePointedTo << "\n";
+	cout << "---------------------------\n";
+	const struct RemoteHeapPointer result = {actualAddr, addressPointedTo, sizePointedTo};
+	matches.push_back(result);
+      }
+
+    } else if (AddrIsInText(addressPointedTo)) {
+      std::cout << "Frame end found return addr to :" << addressPointedTo << "\n";
+      framesSearched++;
+      if (framesSearched > framesToSearch) {
+	break;
+      }
+    }
+  }
+  delete[] stackCopy;
+  return matches;
 }
