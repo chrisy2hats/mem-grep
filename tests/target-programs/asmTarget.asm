33a17f6a11a69125fd20c89ccd02727dd42e8afc
chrisfoster4 <chrismarkfoster@yahoo.co.uk>
2020-01-04 16:57:24 +0000

    UNITTEST Update target asm program to not rely on STDIN existing
    The unit test was failing in Jenkins because it was attempting to open STDIN when the getchar() syscall was invoked. However Jenkins calls the programs from a non interactive terminal so it was failing.
    
    The program now just waits 5 seconds which is enough time for the unit test to run

--- tests/target-programs/asmTarget.asm
+++ tests/target-programs/asmTarget.asm
@@ -1,10 +1,10 @@
-;Move some data around and call read from the terminal
-;Equivilent of Cs getchar()
-;Will be killed by it's parent thread in the unit test
+;Move some data around. Wait 5 seconds then exit
+;Will be killed by it's parent thread in the unit test before 5 seconds most likely
 
 SYS_IN equ 0
 SYS_EXIT equ 60
 STDIN equ 0
+SLEEP equ 35
 
 %macro exit 1
     mov rax,SYS_EXIT
@@ -12,16 +12,19 @@ STDIN equ 0
     syscall
 %endmacro
 
+section .data
+	delay dq 5, 500000000
+
 section .text 
 	global _start
 
 _start:
+	;Do some work. This doesn't actually achieve anything
 	mov r8,127
 	mov [rsp-24],r8
 
-	mov rax,SYS_IN
-	mov rdi,STDIN
-	mov rsi,5
-	mov rdx,5
+	mov rax,SLEEP
+	mov rdi,delay
+	mov rsi,0
 	syscall
 	exit 0
