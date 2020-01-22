2093aaa5162b029acf9dbd2cb18870729d4dabff
chrisfoster4 <chrismarkfoster@yahoo.co.uk>
2020-01-22 12:31:40 +0000

    Make HeapFilter::FlattenAndFilter multi threaded by default
    Previously it was single threaded by default. A single threaded implementation is still provided and is utilised to test the correctness of the multi threaded implementation.

--- src/filtering/heap-filter.cpp
+++ src/filtering/heap-filter.cpp
@@ -4,23 +4,66 @@ std::vector<RemoteHeapPointer> HeapFilter::FlattenAndFilter(
 		const std::vector<RemoteHeapPointer>& base_pointers,
 		const std::function<bool(const RemoteHeapPointer&)>& functor) {
 
+  const unsigned int hardware_thread_count = std::thread::hardware_concurrency();
+  std::vector<std::future<std::vector<RemoteHeapPointer>>> thread_futures(hardware_thread_count);
+  std::vector<RemoteHeapPointer> all_thread_matches = {};
+
+  auto callable_worker =
+		  [](const std::vector<RemoteHeapPointer>& base_pointers,
+				  std::atomic<uint64_t>& shared_index,
+				  const std::function<bool(const RemoteHeapPointer&)>& functor)
+		  -> std::vector<RemoteHeapPointer> {
+    return WorkerThread(base_pointers, shared_index, functor);
+  };
+
+  std::atomic<uint64_t> shared_index(0);
+  for (auto& one_threads_future : thread_futures) {
+    one_threads_future = std::async(std::launch::async, callable_worker, std::ref(base_pointers),
+		    std::ref(shared_index), functor);
+  }
+  for (auto& future : thread_futures) {
+    const std::vector<RemoteHeapPointer> futures_matches = future.get();
+    all_thread_matches.insert(
+		    all_thread_matches.end(), futures_matches.begin(), futures_matches.end());
+  }
+
+  return all_thread_matches;
+}
+
+std::vector<RemoteHeapPointer> HeapFilter::FlattenAndFilterSingleThreaded(
+		const std::vector<RemoteHeapPointer>& base_pointers,
+		const std::function<bool(const RemoteHeapPointer&)>& functor) {
   std::vector<RemoteHeapPointer> matches = {};
 
   for (const auto& ptr : base_pointers) {
-    matches = filter_pointer(ptr, functor, matches);
+    FilterPointer(ptr, functor, matches);
   }
   return matches;
 }
 
-std::vector<RemoteHeapPointer> HeapFilter::filter_pointer(const RemoteHeapPointer& ptr,
+void HeapFilter::FilterPointer(const RemoteHeapPointer& ptr,
 		const std::function<bool(const RemoteHeapPointer&)>& functor,
 		std::vector<RemoteHeapPointer>& matches) {
-
   if (functor(ptr))
     matches.push_back(ptr);
 
   for (const RemoteHeapPointer& i : ptr.contains_pointers_to) {
-    matches = filter_pointer(i, functor, matches);
+    FilterPointer(i, functor, matches);
   }
-  return matches;
+}
+
+std::vector<RemoteHeapPointer> HeapFilter::WorkerThread(
+		const std::vector<RemoteHeapPointer>& base_pointers,
+		std::atomic<uint64_t>& shared_index,
+		const std::function<bool(const RemoteHeapPointer&)>& functor) {
+  std::vector<RemoteHeapPointer> thread_local_matches = {};
+
+  // We must not reference shared_index in the loop as it could have changed in a different thread.
+  // i.e. "base_pointers.at(shared_index)=..." is unsafe and a race condition but
+  // "base_pointers.at(current_local_index) is safe
+  size_t current_local_index = 0;
+  while ((current_local_index = shared_index++) < base_pointers.size()) {
+    FilterPointer(base_pointers.at(current_local_index), functor, thread_local_matches);
+  }
+  return thread_local_matches;
 }
--- src/filtering/heap-filter.hpp
+++ src/filtering/heap-filter.hpp
@@ -1,17 +1,42 @@
 #ifndef MEMGREP_HEAP_FILTER
 #define MEMGREP_HEAP_FILTER
 #include "../misc/utils.hpp"
+#include <future>
 
-class HeapFilter{
- public:
+// Class providing functions to filter the extracted heap by any given condition similar to
+// std::remove_if or filter functions from functional languages.
+
+// This class doesn't provide the code which performs the comparisons to match or not match
+// individual RemoteHeapPointers it only contains the code to walk the heap data structure.
+// The comparision code is passed in as a callable i.e. a lambda
+class HeapFilter {
+  public:
   // All RemoteHeapPointers that meet the criteria specified are returned in one single vector.
   // The hierarchy of the heap is lost
-  static std::vector<RemoteHeapPointer> FlattenAndFilter(const std::vector<RemoteHeapPointer>& base_pointers,
-  		const std::function<bool(const RemoteHeapPointer&)>& functor);
+  // This function uses all available threads the hardware has
+  static std::vector<RemoteHeapPointer> FlattenAndFilter(
+		  const std::vector<RemoteHeapPointer>& base_pointers,
+		  const std::function<bool(const RemoteHeapPointer&)>& functor);
 
- private:
-	static std::vector<RemoteHeapPointer> filter_pointer(const RemoteHeapPointer& ptr,
-			const std::function<bool(const RemoteHeapPointer&)>& functor,
-			std::vector<RemoteHeapPointer>& matches);
+  // Same as the above function except it only uses a single thread.
+  // It may be faster if operating on a very small heap
+  static std::vector<RemoteHeapPointer> FlattenAndFilterSingleThreaded(
+		  const std::vector<RemoteHeapPointer>& base_pointers,
+		  const std::function<bool(const RemoteHeapPointer&)>& functor);
+
+  private:
+  // Recursively follow a single pointer finding matches
+  // The function is void as the matches are inserted in the vector passed in
+  // This avoids creating a vector for every call to the function and returning it
+  static void FilterPointer(const RemoteHeapPointer& ptr,
+		  const std::function<bool(const RemoteHeapPointer&)>& functor,
+		  std::vector<RemoteHeapPointer>& matches);
+
+  // The function passed to the child worker threads. It simply goes through the base
+  // pointers running FilterPointer on them
+  static std::vector<RemoteHeapPointer> WorkerThread(
+		  const std::vector<RemoteHeapPointer>& base_pointers,
+		  std::atomic<uint64_t>& shared_index,
+		  const std::function<bool(const RemoteHeapPointer&)>& functor);
 };
-#endif // MEMGREP_HEAP_FILTER
+#endif	// MEMGREP_HEAP_FILTER
