72e6509d8785c93228ef0b311a39c2aad696772f
Chris Foster <chrismarkfoster@yahoo.co.uk>
2022-12-27 20:44:02 +0000

    Add linked-list example

---
+++ examples/linked_list/analyser.cpp
@@ -0,0 +1,54 @@
+#include "../../lib/heap-traversing/stack-searcher.hpp"
+#include "../../lib/datastructure-detection/linkedlist_detection.hpp"
+#include "../../lib/datastructure-detection/vector_detector.hpp"
+#include "../../lib/misc/prerun-checks.hpp"
+#include <iostream>
+#include <cstdlib>
+
+int main(int argc, char** argv) {
+  if (!CanRun()) {
+    exit(2);
+  }
+
+  if (argc != 2) {
+    std::cout << "Please provide the target programs PID" << std::endl;
+    exit(2);
+  }
+
+  const pid_t pid = stoi(std::string(argv[1]));
+  std::cout << "Analysing " << pid << "\n";
+
+  auto parser = MapParser(pid);
+  auto entries = parser.ParseMap();
+  const MapsEntry stack = parser.getStoredStack();
+  const MapsEntry text = parser.getStoredText();
+  const MapsEntry heap = parser.getStoredHeap();
+  const MapsEntry bss = parser.getStoredBss();
+
+  auto stackSearch = StackSearcher(stack.start, text, pid, 10000);
+  std::vector<RemoteHeapPointer> stack_to_heap_pointers =
+		  stackSearch.findHeapPointers(stack.end, heap, 10000);
+
+  std::cout << "Found " << stack_to_heap_pointers.size()
+	    << " pointers from the stack to the heap\n";
+
+  auto heap_traverser = HeapTraverser(pid, parser.getStoredHeap(), 250000);
+
+  auto traversed = heap_traverser.TraversePointers(stack_to_heap_pointers);
+
+  RemoteHeapPointer ll_head;
+  size_t most_subpointers = 0;
+
+  for (const auto& base_pointer : traversed) {
+    if (base_pointer.total_sub_pointers > most_subpointers) {
+      most_subpointers = base_pointer.total_sub_pointers;
+      ll_head = base_pointer;
+    }
+  }
+
+  auto value_substitutions = std::vector<Substitution>{Substitution{.from = 0, .to = 100000}};
+  std::cout << "Modifiy data in head of linked list malloc'd at " << ll_head.points_to << "\n";
+  RemoteMemory::Substitute(pid, ll_head, value_substitutions);
+
+  return 0;
+}
---
+++ examples/linked_list/target.c
@@ -0,0 +1,49 @@
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+struct LL {
+  struct LL* next;
+  uint32_t data;
+};
+
+struct LL* const init_ll(const size_t length) {
+  struct LL* const head = (struct LL*)calloc(1, sizeof(struct LL));
+
+  struct LL* node = (struct LL*)calloc(1, sizeof(struct LL));
+  head->next = node;
+
+  for (uint32_t i = 0; i < length; i++) {
+    struct LL* next = (struct LL*)calloc(1, sizeof(struct LL));
+    node->next = next;
+    node->data = i;
+    node = next;
+  }
+  return head;
+}
+
+void check_ll(struct LL* head, const size_t length) {
+  for (uint32_t i = 0; i < length; i++) {
+    if (head->data > length) {
+      printf("LL has been modified at node %d. Data is now %d", i, head->data);
+      exit(0);
+    }
+  }
+}
+
+int main() {
+  const pid_t pid = getpid();
+  printf("Out PID is %d\n", pid);
+  const size_t length = 5000;
+  struct LL* ll = init_ll(length);
+
+  printf("Initialised LinkedList of %lu long\n", length);
+
+  while (1) {
+    check_ll(ll, length);
+    sleep(1);
+  }
+
+  return 0;
+}
