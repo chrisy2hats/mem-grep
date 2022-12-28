bee9fd9d74e704aa119e5fc3303ec163c0ba6786
Chris Foster <chrismarkfoster@yahoo.co.uk>
2022-12-28 14:19:12 +0000

    Add test for visited-tracker

---
+++ tests/visited-tracker-tests.cpp
@@ -0,0 +1,24 @@
+#include <catch2/catch.hpp>
+#include <cstring>
+#include <unistd.h>
+#include "../lib/misc/remote-memory.hpp"
+#include "../lib/heap-traversing/visited-tracker.hpp"
+
+TEST_CASE("Visited Tracker") {
+
+  const size_t START = 1000000;
+  const size_t END =   1001024;
+  const size_t size = END-START;
+
+  const MapsEntry fake_heap = MapsEntry{.start=(void*)START,.end=(void*)END,.permissions="",.offset="",.device="",.inode="",.file_path="",.size=size};
+
+  auto vt = VisitedTracker(fake_heap);
+
+  for (auto i=0;i<size;i++){
+    void* addr = (void*) (START+i);
+    REQUIRE_FALSE(vt.IsAlreadyVisited(addr));
+    vt.SetAlreadyVisited(addr);
+    REQUIRE(vt.IsAlreadyVisited(addr));
+  }
+
+}
