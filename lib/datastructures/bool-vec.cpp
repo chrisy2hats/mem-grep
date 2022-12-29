b7c05b333daf4bcffb691019f7bb3e98463b370f
Chris Foster <chrismarkfoster@yahoo.co.uk>
2022-12-29 11:00:40 +0000

    Use unique_ptr for BoolBec internal buffer

--- lib/datastructures/bool-vec.cpp
+++ lib/datastructures/bool-vec.cpp
@@ -1,18 +1,13 @@
 #include "bool-vec.hpp"
 
 BoolVec::BoolVec(const size_t size) {
-  storage_ = new uint8_t[size/8]();
-}
-
-BoolVec::~BoolVec(){
-    delete[] storage_;
+  storage_ = std::make_unique<uint8_t[]>(size/8);
 }
 
 void BoolVec::set_bit(const size_t index) {
   const size_t byte_in_block = index/8;
   const uint8_t bit_in_byte = (index%8);
-  
-  
+
   storage_[byte_in_block] |= BIT_VALUES[bit_in_byte];
 }
 
--- lib/datastructures/bool-vec.hpp
+++ lib/datastructures/bool-vec.hpp
@@ -4,13 +4,13 @@
 #include <array>
 #include <cstdint>
 #include <cstddef>
+#include <memory>
 
 // Simple container that is like a std::vector<bool> except it is thread safe
 // with no syncronisation where a std::vector<bool> may grow when one thread push_backs
 class BoolVec {
   public:
   explicit BoolVec(const size_t max_index);
-  ~BoolVec();
   void set_bit(size_t index);
   [[nodiscard]]  bool is_set(size_t index) const;
 
@@ -19,7 +19,7 @@ class BoolVec {
   BoolVec(BoolVec&) = delete;
 
   private:
-  uint8_t* storage_;
+  std::unique_ptr<uint8_t[]> storage_;
   
   // TODO make endian aware
   const std::array<uint8_t,8> BIT_VALUES = {1,2,4,8,16,32,64,128};
